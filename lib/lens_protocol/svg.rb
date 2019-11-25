21099e4cc164daba6d350f88e072f822db10b4b2
Emmanuel Nicolau <emmanicolau@gmail.com>
2019-11-25 18:58:37 -0300

    Add a center cross to the svg

--- lib/lens_protocol/svg.rb
+++ lib/lens_protocol/svg.rb
@@ -6,19 +6,20 @@ module LensProtocol
       message.tracing_in_rectangular_coordinates.map { |coordinates| from_rectangular_coordinates coordinates, **opts }
     end
 
-    def from_rectangular_coordinates coordinates, **polygon_opts
+    def from_rectangular_coordinates coordinates, polygon: {}, cross_size: 10
       polygon_opts = {
         stroke: 'black',
         stroke_width: 2,
         stroke_linejoin: 'round',
         fill: 'red',
-        points: polygon_points_from_coordinates(coordinates),
-        transform: 'translate(250, 250)'
-      }.merge(polygon_opts)
+        points: polygon_points_from_coordinates(coordinates)
+      }.merge(polygon)
 
       Nokogiri::XML::Builder.new do |xml|
         xml.svg(width: '100%', xmlns: 'http://www.w3.org/2000/svg') do |xml|
           xml.polygon polygon_opts
+          xml.line x1: 0, y1: -cross_size, x2: 0, y2: cross_size
+          xml.line x1: -cross_size, y1: 0, x2: cross_size, y2: 0
         end
       end.doc.root.to_s
     end
